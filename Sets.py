596614f77e634cb8ab764313ee60a78cf52173e5
Mulaga-SaiKrishnaJahnavi <85406829+Mulaga-SaiKrishnaJahnavi@users.noreply.github.com>
2021-06-21 22:02:27 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,7 @@
+E={0,8,6,2,4}
+N={1,3,5,2,4}
+print("Union of E and N is ",E.union(N))
+print("Intersection of E and N is ",E.intersection(N))
+print("Difference of E and N is ",E.difference(N))
+print("Symmetric difference of E and N is ",E.symmetric_difference(N))
+    
